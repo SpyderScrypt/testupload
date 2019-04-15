3dbdb8e4826dc5838b16ad9aa4386e058d307a66
SpyderScrypt <spiderscrypt@gmail.com>
2019-04-15 18:40:39 +0530

    Router Test 2

--- src/router/Router.js
+++ src/router/Router.js
@@ -2,11 +2,15 @@ import React, { Component } from "react";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 import App from "../App";
 import PageNotFound from "../components/PageNotFound/PageNotFound";
+import createHistory from 'history/createBrowserHistory';
 
 class Router extends Component {
   render() {
+    const history = createHistory({
+      basename: process.env.PUBLIC_URL
+    });
     return (
-      <BrowserRouter basename={process.env.PUBLIC_URL}>
+      <BrowserRouter history={history} basename={process.env.PUBLIC_URL}>
         <Switch>
           <Route exact path="/" component={App} />
           <Route exact path="/unknown" component={PageNotFound} />
